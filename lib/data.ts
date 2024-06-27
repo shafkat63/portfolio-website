07a759d300353a65a92b3f8cd2e8a3f1644efd61
Muhtasir Shafkat <67758247+shafkat63@users.noreply.github.com>
2024-06-27 11:17:44 +0600

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -38,24 +38,21 @@ export const experiencesData = [
 		title: "Graduated University",
 		location: "Dhaka, Bangladesh",
 		description:
-			"I graduated with my BSc in Computer Science and Engineering. After That I start creating project for some clients.",
-		icon: React.createElement(LuGraduationCap),
+			"I graduated with a BSc in Computer Science and Engineering. After that, I began developing projects for various clients."		icon: React.createElement(LuGraduationCap),
 		date: "02/02/2023",
 	},
 	{
 		title: "Full-Stack Developer",
 		location: "Dhaka, Bangladesh",
-		description:
-			"I've worked as a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma, and MongoDB.",
-		icon: React.createElement(FaReact),
+		description: 
+			"I've worked as a full-stack developer and freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma, and MongoDB."		icon: React.createElement(FaReact),
 		date: "2023 - 2024",
 	},
 		{
 		title: "Junior Full-Stack Developer",
 		location: "Dhaka, Bangladesh",
 		description:
-			"I'm now a full-stack developer working At System Resources Limited. My stack includes React, Next.js, TypeScript, Tailwind, PHP, Laravel, MySQL, and MongoDB.",
-		icon: React.createElement(FaReact),
+			"I'm currently a full-stack developer at System Resources Limited. My tech stack includes React, Next.js, TypeScript, Tailwind, PHP, Laravel, MySQL, and MongoDB."		icon: React.createElement(FaReact),
 		date: "2024 - present",
 	},
 ] as const;
@@ -64,15 +61,15 @@ export const projectsData = [
 	{
 		title: "newsRoomCMS",
 		description:
-			"I worked as a full-stack developer on this startup project.It was build on MERN stack.It's a factCheck website made for factwatchBD(CQS).",
+			"I worked as a full-stack developer on this startup project.It was built on MERN stack. It's a factcheck website made for factwatchBD(CQS).",
 		tags: ["MongoDB", "Express", "React", "Node.js", "MUI"],
 		imageUrl: newsRoomCMS,
 	},
 	{
 		title: "SubscriptionBox",
 		description:
-			"It was a FullProject build with PHP. I build it for Web Technology course. ",
-		tags: ["HTML", "CSS", "JavaScript", "PHP", "Bootstrape"],
+			"It was a FullProject build with PHP. I build it for the Web Technology course. ",
+		tags: ["HTML", "CSS", "JavaScript", "PHP", "Bootstrap"],
 		imageUrl: SubscriptionBox,
 	},
 	{
@@ -100,5 +97,6 @@ export const skillsData = [
 	"Python",
 	"Php",
 	"Laravel",
+	"MySQL"
 	"Framer Motion",
 ] as const;
